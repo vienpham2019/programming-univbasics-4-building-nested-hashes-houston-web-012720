1f7713295644f6da1a84e8ea99e1eb96cbe9a065
vien pham <vienpham2019@gmail.com>
2019-12-10 03:15:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,7 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	my_base_hash = {
-	  :north_america =>{},
-	  :africa => {}
-	}
+	my_base_hash = {}
 	return my_base_hash
 end
 
@@ -13,7 +10,11 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  
+  my_base_hash = {
+	  :north_america =>{},
+	  :africa => {}
+	}
+	return my_base_hash
 end
 
 def monopoly_with_third_tier
@@ -21,7 +22,17 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  
+  my_base_hash = {
+	  :north_america =>{
+	    "Canada" => {},
+	    "USA" => {}
+	  },
+	  :africa => {
+	    "Ghana" => {},
+	    "Nigeria" => {}
+	  }
+	}
+	return my_base_hash
 end
 
 def monopoly_with_fourth_tier
@@ -29,4 +40,18 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  my_base_hash = {
+	  :north_america =>{
+	    "Canada" => {
+	      :capital => "Ottawa",
+	      :capital_climate => ""
+	    },
+	    "USA" => {}
+	  },
+	  :africa => {
+	    "Ghana" => {},
+	    "Nigeria" => {}
+	  }
+	}
+	return my_base_hash
 end
