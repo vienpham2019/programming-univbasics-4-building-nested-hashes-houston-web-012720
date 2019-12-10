1314e3444d7d9192cf78a968218b6c6987bbe072
vien pham <vienpham2019@gmail.com>
2019-12-10 03:42:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,7 @@ def monopoly_with_third_tier
       :names => {
         :reading_railroad => {},
         :pennsylvania_railroad => {},
-        :b_and_o_rairoad => {},
+        :b_and_o_railroad => {},
         :shortline_railroad => {}
       }
     }
