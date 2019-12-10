36a93a3008e7ca7d0394f8877437326c304eb6d6
vien pham <vienpham2019@gmail.com>
2019-12-10 03:10:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,10 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{}
+	my_base_hash = {
+	  :north_america =>{},
+	  :africa => {}
+	}
 end
 
 def monopoly_with_second_tier
