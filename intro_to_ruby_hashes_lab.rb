196a4685fbdff843a8917183fb9d1b6c2356c55f
vien pham <vienpham2019@gmail.com>
2019-12-10 03:23:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,6 +23,10 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  my_base_hash = {
+    :railroads => {:pieces => 4}
+    :rent_in_dollars => {}
+  }
 	return my_base_hash
 end
 
