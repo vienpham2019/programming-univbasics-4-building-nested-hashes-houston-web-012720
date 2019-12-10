8121540d0d02938feb36bce83194c868e80497e8
vien pham <vienpham2019@gmail.com>
2019-12-10 03:30:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,10 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :rent_in_dollars => {
-        
+        :one_pieces_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
+        :four_pieces_owned => 200,
       }}
   }
 	return my_base_hash
