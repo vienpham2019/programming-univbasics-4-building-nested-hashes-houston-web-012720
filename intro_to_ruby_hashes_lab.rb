55afac3ad567cb0a969f34c8168c36890abb562a
vien pham <vienpham2019@gmail.com>
2019-12-10 03:31:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,9 @@ def monopoly_with_third_tier
         :two_pieces_owned => 50,
         :three_pieces_owned => 100,
         :four_pieces_owned => 200
-      }}
+      },
+      :name => {}
+    }
   }
 	return my_base_hash
 end
