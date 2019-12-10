0dd8f619b46a7220be382c9071e474e7593ffad6
vien pham <vienpham2019@gmail.com>
2019-12-10 03:17:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,7 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	my_base_hash = {}
-	return my_base_hash
+	return Hash.new
 end
 
 def monopoly_with_second_tier
